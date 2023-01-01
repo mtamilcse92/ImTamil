28bf18996e908be8c07dbe10700691102aeedda9
Tamil <mtamilcse92@gmail.com>
2023-01-01 18:51:36 +0530

    fix: responsive change

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -15,17 +15,17 @@ export default function Layout({ children }: LayoutProps) {
         <div className='flex p-3 py-5 justify-end items-start'>
           <ThemeToggle />
         </div>
-        <section className="flex flex-col bg-white dark:bg-zinc-900 flex-1 ">
+        <section className="flex flex-col bg-white dark:bg-zinc-900 md:flex-1 ">
             {children}
-          <div className="absolute bg-white flex-col dark:bg-zinc-900 bottom-0 text-right w-full">
-            <footer className="flex gap-3 p-3 items-center justify-end w-full">
-              <a target="_blank" href='mailto:mtamilcse92@gmail.com' rel="noreferrer"><Image src={GmailLogo} width={42} height={42} alt="" /></a>
-              <a target="_blank" href='https://twitter.com/tamil_92' rel="noreferrer"><Image src={TwitterLogo} width={42} height={42} alt="" /></a>
-              <a target="_blank" href='https://www.linkedin.com/in/tamil-arasan-5722b6171/' rel="noreferrer"><Image src={LinkedinLogo} width={42} height={42} alt="" /></a>
-              <a target="_blank" href='https://github.com/mtamilcse92' rel="noreferrer"><Image src={GithubLogo} width={42} height={42} alt="" /></a>
-            </footer>
-          </div>
-      </section>
+        </section>
+        <div className="absolute bg-white flex-col dark:bg-zinc-900 bottom-0 text-right w-full">
+          <footer className="flex gap-3 p-3 items-center justify-end w-full">
+            <a target="_blank" href='mailto:mtamilcse92@gmail.com' rel="noreferrer"><Image src={GmailLogo} width={42} height={42} alt="" /></a>
+            <a target="_blank" href='https://twitter.com/tamil_92' rel="noreferrer"><Image src={TwitterLogo} width={42} height={42} alt="" /></a>
+            <a target="_blank" href='https://www.linkedin.com/in/tamil-arasan-5722b6171/' rel="noreferrer"><Image src={LinkedinLogo} width={42} height={42} alt="" /></a>
+            <a target="_blank" href='https://github.com/mtamilcse92' rel="noreferrer"><Image src={GithubLogo} width={42} height={42} alt="" /></a>
+          </footer>
+        </div>
       </main>
     )
   }
