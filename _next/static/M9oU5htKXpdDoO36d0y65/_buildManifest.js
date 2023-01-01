51d47ea86038930c2f6548ea4d8c09f3d3b9786f
mtamilcse92 <mtamilcse92@gmail.com>
2023-01-01 13:00:58 +0000

    Deploying to page-deploy from @ mtamilcse92/ImTamil@bc4bac2f8c977a2cd791560733cc9e360075a1d9 🚀

--- _next/static/M9oU5htKXpdDoO36d0y65/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-bc1eeae5740f1490.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
