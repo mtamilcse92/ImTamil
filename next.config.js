fcf02e4a77c8c10fb2e4f7a5f2379c1d99d71f0a
Tamil <mtamilcse92@gmail.com>
2023-01-01 16:56:18 +0530

    feat: script change

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: {
+    loader: "akamai",
+    path: "public"
+  }
 }
 
 module.exports = nextConfig
